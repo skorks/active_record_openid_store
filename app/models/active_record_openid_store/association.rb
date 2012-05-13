18b2f30cae9f5ea71aa94a2787363cba73ed39e7
Bente Pieck <b.pieck@x-ion.de>
2012-05-13 02:36:30 +0200

    fixed mass-assignment error

--- app/models/active_record_openid_store/association.rb
+++ app/models/active_record_openid_store/association.rb
@@ -1,11 +1,12 @@
-require 'openid/association'
+require 'openid/association' 
 require 'time'
 require 'active_record'
 
 class ActiveRecordOpenidStore::Association < ActiveRecord::Base
   self.table_name= 'open_id_associations'
+  attr_accessible :server_url, :handle, :secret, :issued, :lifetime, :assoc_type
   def from_record
     OpenID::Association.new(handle, secret, Time.at(issued), lifetime, assoc_type)
   end
 end
-
+,
--- app/models/active_record_openid_store/nonce.rb
+++ app/models/active_record_openid_store/nonce.rb
@@ -2,4 +2,5 @@ require 'active_record'
 
 class ActiveRecordOpenidStore::Nonce < ActiveRecord::Base
   self.table_name= 'open_id_nonces'
+  attr_accessible :server_url, :timestamp, :salt
 end
